aa4e95e86ff83fc173c6bfc66eb43cb7acad5bdd
archana.mayani <archana.mayani@gmail.com>
2024-07-24 00:06:14 -0700

    1. Fixed routing 2. Fixed CORS

--- src/components/Content.vue
+++ src/components/Content.vue
@@ -1,35 +1,33 @@
 <template>
     <div>
-      <h2>Content</h2>
-      <div v-if="content">
-        <p>{{ content }}</p>
-      </div>
-      <div v-else>
-        <p>Loading...</p>
-      </div>
+        <h2>Content</h2>
+        <div v-if="content">
+            <p>{{ content }}</p> 
+            <!-- Ideally, response data needs to be parsed and assign to various DOM elements in order to display the formatted content -->
+        </div>
+        <div v-else>
+            <p>"To err is human; to blame it on the software is even more human!"</p>
+        </div>
+
     </div>
-  </template>
+</template>
 
-  <script>
-  import axios from 'axios';
-  import { ref, onMounted } from 'vue';
+<script setup>
+import axios from 'axios';
+import { ref, onMounted } from 'vue';
 
-  export default {
-    setup() {
-      const content = ref(null);
+const content = ref(null);
 
-      onMounted(() => {
-        axios.get('http://localhost:8080/content')
-          .then(response => {
-            content.value = response.data;
-          }).catch(error => {
-            alert('Error fetching content: ' + error);
-          });
-      });
+onMounted(() => {
+    axios.get('http://localhost:8080/restricted/content', {
+        headers: {
+            "Authorization": 'Bearer ' + localStorage.getItem("authToken")
+        }
+    }).then(response => {
+        content.value = response.data;
+    }).catch(error => {
+        alert('Error fetching content: ' + error);
+    });
+});
 
-      return {
-        content,
-      };
-    },
-  };
-  </script>
+</script>
